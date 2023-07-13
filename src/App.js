367504e2e4b5855a3f7dd758c96d2246d908075c
marcusguay <55104472+marcusguay@users.noreply.github.com>
2023-07-12 17:33:49 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ async loadModel(){
 
 
   if(!model) 
-    model = await tf.loadLayersModel('model.json');
+    model = await tf.loadLayersModel('https://chinese-character-server.onrender.com/model.json');
   }
 
 
