1f739547ef03383e338cc3c666e632cd8ac48ff6
marcusguay <marcus.guay.99@gmail.com>
2023-07-12 17:50:01 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -47,13 +47,13 @@ this.loadModel();
 async loadModel(){
 
 
-  const response = await fetch('https://chinese-character-server.onrender.com/GetCharData');
+  const response = await fetch('/GetCharData');
   const json = await response.json();
   charArray = json['array'];
 
 
   if(!model) 
-    model = await tf.loadLayersModel('https://chinese-character-server.onrender.com/model.json');
+    model = await tf.loadLayersModel('model.json');
   }
 
 
@@ -94,7 +94,7 @@ async loadModel(){
       });
 
           var int = 0;
-          var Request = 'https://chinese-character-server.onrender.com/CharSearch/?char='
+          var Request = '/CharSearch/?char='
       array.forEach( async element =>{
         const character = charArray[element];
         console.log(character);  
